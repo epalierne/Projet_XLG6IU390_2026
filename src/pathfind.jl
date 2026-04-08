5cc3b3c64e71c53b52d56e1c540784dd2d1cb5d9
epalierne <erwan.palierne@gmail.com>
2026-04-08 23:37:25 +0200

    Modification de l'affichage

--- src/pathfind.jl
+++ src/pathfind.jl
@@ -1,5 +1,5 @@
 #= Auteur : PALIERNE Erwan (E215834C) ; groupe 681C 
-   Rôle : 
+   Rôle : Fonctions appelant chacun des quatre algorithmes
 =#
 
 include("graphe.jl")
@@ -12,16 +12,16 @@ include("Glouton.jl")
 function algoBFS(fname::String, D::Tuple{Int,Int}, A::Tuple{Int,Int})
     graphe = creation_du_graphe(fname)
     if !haskey(graphe, D) || !haskey(graphe, A)
-        println("L'un des deux points est impratiquable.")
+        println("Au moins un des deux points est hors de la carte.")
     else
         t = time()
         chemin, nombre_de_sommet, distance = BFS(graphe, D, A)
         dt = round(time() - t, digits=6)
         if chemin != nothing
-            println(" \n\n Distance  D -> A : ", distance)
+            println(" \n\n Distance  D -> A           : ", distance)
             println(" Number of states evaluated : ", nombre_de_sommet)
-            println(" Temps : ", dt, " sec")
-            println(" Path D -> A " )
+            println(" Time (s)                   : ", dt)
+            println(" \n Path D -> A :" )
             for element in chemin
                 print(element)
                 if element != chemin[end] print("->") end 
@@ -37,16 +37,16 @@ end
 function algoDijkstra(fname::String, D::Tuple{Int,Int}, A::Tuple{Int,Int})
     graphe = creation_du_graphe(fname)
     if !haskey(graphe, D) || !haskey(graphe, A)
-        println("L'un des deux points est impratiquable.")
+        println("Au moins un des deux points est hors de la carte.")
     else
         t = time()
         chemin, nombre_de_sommet, distance = Dijkstra(graphe, D, A)
         dt = round(time() - t, digits=6)
         if chemin != nothing
-            println(" \n\n Distance  D -> A : ", distance)
+            println(" \n\n Distance  D -> A           : ", distance)
             println(" Number of states evaluated : ", nombre_de_sommet)
-            println(" Temps : ", dt, " sec")
-            println(" Path D -> A ")
+            println(" Time (s)                   : ", dt)
+            println(" \n Path D -> A :" )
             for element in chemin
                 print(element)
                 if element != chemin[end] print("->") end 
@@ -62,16 +62,16 @@ end
 function algoAstar(fname::String, D::Tuple{Int,Int}, A::Tuple{Int,Int})
     graphe = creation_du_graphe(fname)
     if !haskey(graphe, D) || !haskey(graphe, A)
-        println("L'un des deux points est impratiquable.")
+        println("Au moins un des deux points est hors de la carte.")
     else
         t = time()
             chemin, nombre_de_sommet, distance = Astar(graphe, D, A)
         dt = round(time() - t, digits=6)
         if chemin != nothing
-            println(" \n\n Distance  D -> A : ", distance)
+            println(" \n\n Distance  D -> A           : ", distance)
             println(" Number of states evaluated : ", nombre_de_sommet)
-            println(" Temps : ", dt, " sec")
-            println(" Path D -> A ")
+            println(" Time (s)                   : ", dt)
+            println(" \n Path D -> A :" )
             for element in chemin
                 print(element)
                 if element != chemin[end] print("->") end 
@@ -87,16 +87,16 @@ end
 function algoGlouton(fname::String, D::Tuple{Int,Int}, A::Tuple{Int,Int})
     graphe = creation_du_graphe(fname)
     if !haskey(graphe, D) || !haskey(graphe, A)
-        println("L'un des deux points est impratiquable.")
+        println("Au moins un des deux points est hors de la carte.")
     else
         t = time()
         chemin, nombre_de_sommet, distance = glouton(graphe, D, A)
         dt = round(time() - t, digits=6)
         if chemin != nothing
-            println(" \n\n Distance D -> A : ", distance)
+            println(" \n\n Distance  D -> A           : ", distance)
             println(" Number of states evaluated : ", nombre_de_sommet)
-            println(" Temps : ", dt, " sec")
-            println(" Path D -> A : ")
+            println(" Time (s)                   : ", dt)
+            println(" \n Path D -> A :" )
             for element in chemin
                 print(element)
                 if element != chemin[end] print("->") end 
